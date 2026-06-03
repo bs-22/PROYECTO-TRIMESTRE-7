e862c975e8086d47a18c01a36381ee46959eca28
jcastrocym-hue <jcastrocym@gmail.com>
2026-06-03 17:08:57 -0500

    03/06/2026

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace GestionSemillero1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Muestra la vista del Login al cargar la página
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Recibe los datos cuando el usuario da clic en "Iniciar Sesión"
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string Username, string Password, bool RememberMe = false)
+        {
+            // Validación de prueba (Cambiar luego por tu base de datos)
+            if (Username == "admin" && Password == "1234")
+            {
+                // Si es correcto, viaja al Index del Home (Dashboard)
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Si falla, guardamos el mensaje de error y recargamos la vista
+            ViewBag.Error = "Usuario o contraseña incorrectos.";
+            return View();
+        }
+    }
+}
